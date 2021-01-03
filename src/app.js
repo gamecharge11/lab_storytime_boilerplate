73f43781966bee046f8fd73eda8556b79219da0b
gamecharge11 <73420074+gamecharge11@users.noreply.github.com>
2021-01-03 13:31:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,30 +2,51 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
-
+function moreAboutHome (address, distanceFromTown, hasNeighbours) {
+    var text = typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours);
+    return text
+}
 //Trial 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
-
+function moreAboutKaren (parents, noOfSiblings, isNuclearFamily) {
+    if (typeof(parents) == "string" && typeof(noOfSiblings) == "number" && typeof(isNuclearFamily) == "boolean") {
+        return true
+    }else {
+        return false
+    }
+}
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
-
+function doesFriendExist (ageInText, ageInNumber) {
+    if (typeof(ageInNumber) == "NaN") {
+        return ageInNumber
+    } else {
+        return ageInText
+    }
+}
 //Trial 4:
 //Lily gave Karen x sweets
 //Karen ate y sweets herself
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
-
+function sweetTooth(totalNoOfSweets,sweetsConsumedByKaren,SweetsConsumedInNMeters,metersToTravel){
+    var remaining=(totalNoOfSweets-(sweetsConsumedByKaren+SweetsConsumedInNMeters*metersToTravel))/2;
+    return remaining;
+}
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
-
+function convertToCelsius (fahrenheit) {
+    var ans = (fahrenheit - 32) * 5 / 9;
+    return ans
+}
 //Trial 6:
 //Lily can now do multiple things to deal with this
-//1. Take her daughter to a doctor
+//1. Take her daughter to a doctor 
 //2. Talk to her husband about it
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
